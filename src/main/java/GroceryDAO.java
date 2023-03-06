9b9b45d9b397f8444dd2d684b7a792ebce81a503
abdulkarim-hamidi <abdulkarim.hamidi@yahoo.com>
2023-03-06 21:35:06 +0000

    lab completed

--- src/main/java/GroceryDAO.java
+++ src/main/java/GroceryDAO.java
@@ -8,19 +8,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A DAO is a class that mediates the transformation of data between the format of objects in Java to rows in a
- * database. The methods here are mostly filled out, you will just need to add a SQL statement.
+ * A DAO is a class that mediates the transformation of data between the format
+ *  of objects in Java to rows in a
+ * database. The methods here are mostly filled out, you will just need to add 
+ * a SQL statement.
  *
  * We may assume that the database has already created a table named 'Grocery'.
  * It contains a single column, named 'grocery_name' of type varchar(255).
- * The table will be automatically created by the databaseSetup() method in GroceryMain.java.
+ * The table will be automatically created by the databaseSetup() method in 
+ * GroceryMain.java.
  */
 public class GroceryDAO {
 
 
     /**
      * TODO: Select all of the rows of the Grocery table.
-     * You only need to change the sql String, the rest of the method is already complete.
+     * You only need to change the sql String, the rest of the method is
+     *  already complete.
      * @return a List of all the groceries contained within the database.
      */
     public List<String> getAllGroceries(){
@@ -28,7 +32,7 @@ public class GroceryDAO {
         List<String> groceries = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "select * from Grocery";
             PreparedStatement ps = connection.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
@@ -40,30 +44,39 @@ public class GroceryDAO {
         return groceries;
     }
     /**
-     * TODO: Insert a new row into the Grocery table, which contains a column named grocery_name.
-     * A parameter groceryName is also provided, which should be the value used for grocery_name when inserting a
-     * grocery. For instance, calling groceryDAO.addGrocery("bread"); should insert a record into the grocery table
+     * TODO: Insert a new row into the Grocery table, which contains a column 
+     * named grocery_name.
+     * A parameter groceryName is also provided, which should be the value 
+     * used for grocery_name when inserting a
+     * grocery. For instance, calling groceryDAO.addGrocery("bread"); should
+     *  insert a record into the grocery table
      * with the value 'bread' for the grocery_name column.
      *
-     * You should use the preparedStatement syntax for filling in unknown parameters with a '?'.
+     * You should use the preparedStatement syntax for filling in unknown 
+     * parameters with a '?'.
      * For instance, an insert statement would be written as:
      * String sql = "INSERT INTO TABLE (Column1, Column2) VALUES (?, ?)"
-     * The question marks can be filled in by the methods setString, setInt, etc of the PreparedStatement. they follow
-     * this format, where the first argument identifies the question mark to be filled (left to right, starting
+     * The question marks can be filled in by the methods setString, setInt,
+     *  etc of the PreparedStatement. they follow
+     * this format, where the first argument identifies the question mark to 
+     * be filled (left to right, starting
      * from one) and the second argument identifies the variable to be used:
      * preparedStatement.setString(1,int1);
      * preparedStatement.setInt(2,string2);
      *
-     * @param groceryName the name of the grocery passed in from the GroceryService.
+     * @param groceryName the name of the grocery passed in from the 
+     * GroceryService.
      */
     public void addGrocery(String groceryName){
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "INSERT INTO Grocery (grocery_name) VALUES (?)";
             PreparedStatement ps = connection.prepareStatement(sql);
 
             //add code that leverages ps.setString here
+            ps.setString(1, groceryName);
+            // ps.setInt(2, 3);
 
             ps.executeUpdate();
         }catch(SQLException e){
